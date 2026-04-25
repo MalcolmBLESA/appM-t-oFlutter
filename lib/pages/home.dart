577aee39179ba6a42b12608e9fdd894b76036400
MalcolmBLESA <malcolmblesa@gmail.com>
2026-04-25 15:41:07 +0200

    création de getWeather et connexion à l'api météo + ébauche date picker

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:meteo_aquatech/weather_service.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -17,6 +18,34 @@ class _HomePageState extends State<HomePage> {
   String wind = "--";
   String precipitation = "--";
   String cloud = "--";
+  DateTimeRange? selectedDateRange;
+
+  Future<void> _pickDateRange() async {
+    final DateTimeRange? picked = await showDateRangePicker(
+      context: context,
+      firstDate: DateTime(1900),
+      lastDate: DateTime.now().add(const Duration(days: 14)),
+      saveText: 'Valider',
+    );
+
+    if (picked != null) {
+      setState(() {
+        selectedDateRange = picked;
+      });
+      print("Dates choisies : ${picked.start} au ${picked.end}");
+    }
+  }
+
+
+  Map<String, String> getNextSevenDays() {
+  DateTime now = DateTime.now();
+  DateTime sevenDaysLater = now.add(Duration(days: 6)); // J+6 pour avoir 7 jours au total
+
+  return {
+    'start': now.toIso8601String().split('T')[0],
+    'end': sevenDaysLater.toIso8601String().split('T')[0],
+  };
+}
 
   @override
   Widget build(BuildContext context) {
@@ -60,16 +89,10 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  
+
   Widget _nameCity() {
     return Container(
-      decoration: const BoxDecoration(
-        boxShadow: [
-          BoxShadow(
-            color: Color.fromARGB(50, 255, 255, 255),
-            blurRadius: 40,
-          ),
-        ],
-      ),
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
       child: Text(
         cityName,
@@ -86,14 +109,21 @@ class _HomePageState extends State<HomePage> {
       margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
       decoration: const BoxDecoration(
         boxShadow: [
-          BoxShadow(
-            color: Color.fromARGB(200, 0, 0, 0),
-            blurRadius: 40,
-          ),
+          BoxShadow(color: Color.fromARGB(200, 0, 0, 0), blurRadius: 40),
         ],
       ),
       child: TextField(
-        onSubmitted: (value) async { 
+        onSubmitted: (value) async {
+          String start,end;
+
+          if(selectedDateRange != null){
+            start = selectedDateRange!.start.toIso8601String().split('T')[0];
+            end = selectedDateRange!.end.toIso8601String().split('T')[0];
+          }else{
+            final dates = getNextSevenDays();
+            start = dates['start']!;
+            end = dates['end']!;
+          }
           setState(() {
             cityName = value;
           });
@@ -101,10 +131,32 @@ class _HomePageState extends State<HomePage> {
           final coords = await _weatherService.getCoordinates(value);
 
           if (coords != null) {
-            print("Coordonnées de $value : Lat ${coords['lat']}, Lon ${coords['lon']}");
+            final data = await _weatherService.getWeather(coords['lat']!, coords['lon']!,start,end);
+            print("Latitude : ${coords['lat']}");
+            print("Longitude : ${coords['lon']}");
+            if(data != null){
+
+              String nowHour = DateTime.now().toIso8601String().substring(0, 13) + ":00";
+              List<dynamic> times = data['hourly']['time'];
+              int index = times.indexOf(nowHour);
+              if (index == -1) index = 0;
+
+              setState(() {
+                cityName = value;
+                temperature = data['hourly']['temperature_2m'][index].toString();
+                felt = data['hourly']['apparent_temperature'][index].toString();
+                humidity = data['hourly']['relative_humidity_2m'][index].toString();
+                wind = data['hourly']['wind_speed_10m'][index].toString();
+                precipitation = data['hourly']['precipitation'][index].toString();
+                cloud = data['hourly']['cloud_cover'][index].toString();
+
+
+              });
+            }
           } else {
             print("Ville non trouvée");
           }
+          
         },
         decoration: InputDecoration(
           hintText: 'Rechercher une ville',
@@ -112,8 +164,8 @@ class _HomePageState extends State<HomePage> {
           filled: true,
           fillColor: const Color.fromARGB(250, 255, 255, 255),
           contentPadding: const EdgeInsets.all(0),
-          suffixIcon: SizedBox(
-            width: 50,
+          suffixIcon: GestureDetector(
+            onTap: _pickDateRange, // Appelle la fonction de dates
             child: Padding(
               padding: const EdgeInsets.all(15),
               child: SvgPicture.asset('assets/icons/calendar-days-svgrepo-com.svg'),
@@ -138,12 +190,6 @@ class _HomePageState extends State<HomePage> {
   Widget _buildWeatherCard({required String label, required String value, required String unit}) {
     return Container(
       decoration: BoxDecoration(
-        boxShadow: const [
-          BoxShadow(
-            color: Color.fromARGB(15, 255, 255, 255),
-            blurRadius: 40,
-          ),
-        ],
         color: const Color.fromARGB(50, 255, 255, 255),
         borderRadius: BorderRadius.circular(20),
       ),
@@ -151,23 +197,10 @@ class _HomePageState extends State<HomePage> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(
-            label,
-            style: const TextStyle(color: Colors.white70, fontSize: 12),
-          ),
+          Text(label, style: const TextStyle(color: Colors.white70, fontSize: 12)),
           const SizedBox(height: 5),
-          Text(
-            value,
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Text(
-            unit,
-            style: const TextStyle(color: Colors.white, fontSize: 12),
-          ),
+          Text(value, style: const TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold)),
+          Text(unit, style: const TextStyle(color: Colors.white, fontSize: 12)),
         ],
       ),
     );
@@ -177,45 +210,15 @@ class _HomePageState extends State<HomePage> {
     return AppBar(
       backgroundColor: Colors.transparent,
       elevation: 0,
-      leading: GestureDetector(
-        onTap: () {},
-        child: Container(
-          alignment: Alignment.center,
-          margin: const EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(0, 255, 255, 255),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: SvgPicture.asset(
-            'assets/icons/left-arrow-svgrepo-com (1).svg',
-            height: 20,
-            width: 20,
-          ),
-        ),
+      leading: Padding(
+        padding: const EdgeInsets.all(15),
+        child: SvgPicture.asset('assets/icons/left-arrow-svgrepo-com (1).svg'),
       ),
-      title: const Text(
-        "Météo",
-        style: TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
+      title: const Text("Météo", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
       actions: [
-        GestureDetector(
-          onTap: () {},
-          child: Container(
-            alignment: Alignment.center,
-            margin: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(0, 255, 255, 255),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: SvgPicture.asset(
-              'assets/icons/three-dots-menu-svgrepo-com (1).svg',
-              height: 20,
-              width: 20,
-            ),
-          ),
+        Padding(
+          padding: const EdgeInsets.all(15),
+          child: SvgPicture.asset('assets/icons/three-dots-menu-svgrepo-com (1).svg'),
         ),
       ],
     );
--- lib/weather_service.dart
+++ lib/weather_service.dart
@@ -27,4 +27,16 @@ class WeatherService {
     }
     return null;
   }
+  Future<Map<String, dynamic>?> getWeather(double lat, double lon, String start, String end) async {
+  final url = Uri.parse(
+    'https://api.open-meteo.com/v1/forecast?latitude=$lat&longitude=$lon&start_date=$start&end_date=$end&hourly=temperature_2m,apparent_temperature,relative_humidity_2m,precipitation,cloud_cover,wind_speed_10m&timezone=auto'
+  );
+
+  final response = await http.get(url);
+  if(response.statusCode == 200){
+    return jsonDecode(response.body);
+  }
+  return null;
 }
+}
+
